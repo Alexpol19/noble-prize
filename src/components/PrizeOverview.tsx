71ae0ceb5c3d0e55991a4bbcd512c56754be5ec1
Alexei <leha.pol1996@mail.ru>
2024-07-21 19:44:19 +0300

    impove perf by splitting charts with fetch from the select range

--- src/components/PrizeOverview.tsx
+++ src/components/PrizeOverview.tsx
@@ -1,62 +1,9 @@
-import { PrizeChart } from './PrizeChart'
-import { usePrizes, useLaureates } from '../hooks'
 import { SelectYearRange } from './SelectYearRange'
-import { useMemo, useState } from 'react'
-import { LaureatesNumberChart } from './LaureatesNumberChart';
-import { PrizeAmountChart } from './PrizeAmountChart';
-
-const countCategories = (data: any[]):Record<string, number> => {
-  return data.reduce((acc, item) => {
-    const category = item.category.en;
-    if (acc[category]) {
-      acc[category] += 1;
-    } else {
-      acc[category] = 1;
-    }
-    return acc;
-  }, {});
-};
-
-const groupPrizesByYear = (prizes: any[]):Record<string, any> => prizes.reduce((acc, prize) => {
-  const { awardYear } = prize;
-  if (!acc[awardYear]) {
-      acc[awardYear] = [];
-  }
-  acc[awardYear].push(prize);
-  return acc;
-}, {});
-
-const groupLaureatesByYear = (laureates: any[], yearsRange: number[]):Record<string, any> => {
-  const [minYear, maxYear] = yearsRange;
-
-  return laureates.reduce((acc, laureate) => {
-      laureate.nobelPrizes.forEach((prize: any) => {
-          const { awardYear } = prize;
-          if (awardYear >= minYear && awardYear <= maxYear) {
-              if (!acc[awardYear]) {
-                  acc[awardYear] = [];
-              }
-              acc[awardYear].push(laureate);
-          }
-      });
-      return acc;
-  }, {});
-};
+import { useState } from 'react'
+import PrizeCharts from './PrizeCharts';
 
 const PrizeOverview = () => {
-  const [yearsRange, setYearsRange] = useState([1901, 1909])
-
-  const {
-    data,
-  } = usePrizes(yearsRange[0], yearsRange[1]);
-  const {
-    data: dataLaureates,
-  } = useLaureates(yearsRange[0], yearsRange[1]);
-
-  const categoryAwards = useMemo(() => countCategories(data || []), [data]);
-
-  const groupedPrizesPerYear = useMemo(() => groupPrizesByYear(data || []), [data]);
-  const groupedLaureatesPerYear = useMemo(() => groupLaureatesByYear(dataLaureates || [], yearsRange), [dataLaureates]);
+  const [yearsRange, setYearsRange] = useState<[number, number]>([1901, 1909])
 
   return (
     <div>
@@ -72,23 +19,7 @@ const PrizeOverview = () => {
           }}
         />
       </div>
-      <div className="mb-7" />
-      <PrizeAmountChart
-        title="Adjusted award amount"
-        subtitle={`${yearsRange[0]}-${yearsRange[1]}`}
-        data={groupedPrizesPerYear}
-      />
-      <div className="mb-7" />
-      <LaureatesNumberChart
-        title="Number of laureates"
-        subtitle={`${yearsRange[0]}-${yearsRange[1]}`}
-        data={groupedLaureatesPerYear}
-      />
-      <div className="mb-7" />
-      <PrizeChart
-        subtitle={`${yearsRange[0]}-${yearsRange[1]}`}
-        data={categoryAwards}
-      />
+      <PrizeCharts yearsRange={yearsRange} />
     </div>
     )
 }
